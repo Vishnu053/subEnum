8efae6d6360872a63938f04543b960c7ee985615
vishnu053 <vishnunair053@gmail.com>
2020-07-02 00:11:10 +0530

    changes

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This will create a bashrc alias for quick execution
-echo "Taking a backup of your .bashrc file.."
-cp ~/.bashrc ~/.bashrc-copy-backup
-filepth=`pwd`
-sudo echo "alias subEnum='$filepth/./subEnum.sh'" >> ~/.bashrc
-echo "Path added to your .bashrc file"
-echo "The backup of .bashrc can be found at ~/.bashrc-copy-backup"
